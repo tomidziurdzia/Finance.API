c7b9677fab57752fc743a88b83cdcc94c184531a
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-10-05 13:29:22 +0200

    Pagination

---
+++ src/FinanceApp.Application/Specifications/BaseSpecification.cs
@@ -0,0 +1,41 @@
+using System.Linq.Expressions;
+
+namespace FinanceApp.Application.Specifications;
+
+public class BaseSpecification<T> : ISpecification<T>
+{
+    public BaseSpecification()
+    {
+        
+    }
+    public BaseSpecification(Expression<Func<T, bool>> criteria)
+    {
+        Criteria = criteria;
+    }
+    public Expression<Func<T, bool>>? Criteria { get; }
+    public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+    public Expression<Func<T, object>>? OrderBy { get; private set; }
+    public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+    public int Take { get; private set; }
+    public int Skip { get; private set; }
+    public bool IsPaginationEnable { get; private set; }
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+    
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderByDescending = orderByExpression;
+    }
+    protected void ApplyPagination(int skip, int take)
+    {
+        Skip = skip;
+        Take = take;
+        IsPaginationEnable = true;
+    }
+    protected void AddInclude(Expression<Func<T, object>> includeExpression)
+    {
+        Includes.Add(includeExpression);
+    }
+}
