c77f0d928bab8080d82b32d8ec681a8ca88f5315
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-10-24 21:03:24 +0200

    more changes

--- src/FinanceApp.Application/Features/Wallets/Queries/GetWalletsTotal/GetWalletTotalsQuery.cs
+++ src/FinanceApp.Application/Features/Wallets/Queries/GetWalletsTotal/GetWalletTotalsQuery.cs
@@ -1,6 +1,8 @@
+using FinanceApp.Application.CQRS;
+using FinanceApp.Application.DTOs.Wallet;
+
 namespace FinanceApp.Application.Features.Wallets.Queries.GetWalletsTotal;
 
-public class GetWalletTotalsQuery
+public class GetWalletTotalsQuery : IQuery<WalletTotalsResponseDto>
 {
-    
 }
--- src/FinanceApp.Application/Features/Wallets/Queries/GetWalletsTotal/GetWalletTotalsQueryHandler.cs
+++ src/FinanceApp.Application/Features/Wallets/Queries/GetWalletsTotal/GetWalletTotalsQueryHandler.cs
@@ -1,6 +1,37 @@
+using FinanceApp.Application.Contracts.Identity;
+using FinanceApp.Application.CQRS;
+using FinanceApp.Application.DTOs.Wallet;
+using FinanceApp.Domain.Models;
+using FinanceApp.Domain.Repositories;
+using Microsoft.AspNetCore.Identity;
+
 namespace FinanceApp.Application.Features.Wallets.Queries.GetWalletsTotal;
 
-public class GetWalletTotalsQueryHandler
+public class GetWalletTotalsQueryHandler(
+    IWalletRepository walletRepository,
+    UserManager<User> userManager,
+    IAuthService authService)
+    : IQueryHandler<GetWalletTotalsQuery, WalletTotalsResponseDto>
 {
-    
+    public async Task<WalletTotalsResponseDto> Handle(GetWalletTotalsQuery request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByNameAsync(authService.GetSessionUser());
+        if (user == null) throw new UnauthorizedAccessException("User not authenticated");
+
+        var wallets = await walletRepository.GetAll(user.Id, cancellationToken);
+
+        var walletTotals = wallets.Select(wallet => new WalletTotalDto
+        {
+            Id = wallet.Id,
+            Total = wallet.Transactions.Sum(t => t.Amount)
+        }).ToList();
+
+        var totalSum = walletTotals.Sum(w => w.Total);
+
+        return new WalletTotalsResponseDto
+        {
+            Wallets = walletTotals,
+            Total = totalSum
+        };
+    }
 }
