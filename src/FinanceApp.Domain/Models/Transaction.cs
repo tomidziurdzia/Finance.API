a866f2fd5d99f211eca6cc29d6c496b000d98557
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-10-28 18:32:16 +0100

    change date

--- src/FinanceApp.Domain/Models/Transaction.cs
+++ src/FinanceApp.Domain/Models/Transaction.cs
@@ -14,4 +14,5 @@ public class Transaction : Entity
     public TransactionType Type { get; set; }
     public decimal Amount { get; set; }
     public string? Description { get; set; }
+    public DateTime? CreatedAt { get; set; }
 }
