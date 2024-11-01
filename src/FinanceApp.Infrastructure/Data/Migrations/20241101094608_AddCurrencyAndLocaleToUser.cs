ee26f4d1cf8cae35bedb8d10b0df95b878fd41bf
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-11-01 10:55:26 +0100

    db

---
+++ src/FinanceApp.Infrastructure/Data/Migrations/20241101094608_AddCurrencyAndLocaleToUser.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceApp.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrencyAndLocaleToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Locale",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Locale",
+                table: "AspNetUsers");
+        }
+    }
+}
