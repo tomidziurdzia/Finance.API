10662314c4f05abfb30139c600c65528aa71c88a
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-11-02 11:07:43 +0100

    create PR

--- src/FinanceApp.Application/DTOs/Wallet/WalletTransactionsDto.cs
+++ src/FinanceApp.Application/DTOs/Wallet/WalletTransactionsDto.cs
@@ -3,6 +3,7 @@ namespace FinanceApp.Application.DTOs.Wallet;
 public class WalletTransactionsDto
 {
     public Guid Id { get; set; }
+    public Guid? CategoryId { get; set; }
     public string CategoryName { get; set; }
     public string Type { get; set; }
     public decimal Amount { get; set; }
